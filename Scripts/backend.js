f3039fd284a29262a144e565ba701dfd0ce535b0
Lukas Urba <lukas.urba@gmail.com>
2022-02-21 16:17:53 +0000

    Added static file middleware

--- Scripts/backend.js
+++ Scripts/backend.js
@@ -1,14 +1,39 @@
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
+const req = require("express/lib/request");
 
 var app = express();
-var publicPath = path.join(__dirname, "../public/");
-app.use(express.static(publicPath));
 
 app.use(function (request, response, next) {
     console.log("Request IP: " + request.url);
     console.log("Request date: " + new Date());
     next();
+});
+
+// var imagePath = path.resolve(__dirname,"../Images");
+// app.use(express.static(imagePath));
+
+app.use(function (request, response, next) {
+    var filePath = path.join(__dirname,"/../Images/", request.url);
+    console.log(filePath);
+    fs.stat(filePath, function (error, fileInfo) {
+        if (error) {
+            response.send("This file does not exist.");
+            next();
+            return;
+        }
+        if (fileInfo.isFile()) response.sendFile(filePath);
+        else next();
+    });
+});
+
+app.get("/", function (request, response) {
+    response.sendFile(path.join(__dirname, '/../index.html'));
+});
+
+app.use(function (request, response) {
+    response.status(404).send("This page has not been made yet!");
 });
 
 app.listen(3000, function () {
