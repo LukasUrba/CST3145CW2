db7351e1b4a7e193004a38d776260622c2944909
Lukas Urba <lukas.urba@gmail.com>
2022-02-22 00:05:23 +0000

    most current build to test for heroku

--- Scripts/backend.js
+++ Scripts/backend.js
@@ -3,31 +3,46 @@ var path = require("path");
 var fs = require("fs");
 const req = require("express/lib/request");
 const cors = require("cors");
-const MongoClient = require("mongodb").MongoClient;
-
-var app = express();
+const mongoClient = require("mongodb").MongoClient;
+const app = express();
 
 app.use(cors());
 app.use(express.json())
 
+// mongodb+srv://lukas-urba:123123456aA.@cst3145.grrhk.mongodb.net/
+// mongodb://localhost:3000/
 let db;
-MongoClient.connect('mongodb+srv://lukas-urba:726694222462aA.@cst3145.grrhk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', (err, client) => {
-    db = client.db('webstore')
+mongoClient.connect('mongodb+srv://lukas-urba:123123456aA.@cst3145.grrhk.mongodb.net/', (error, client) => {
+    db = client.db('webstore');
+});
+
+app.param('collectionName', (req, response, next, collectionName) => {
+    req.collection = db.collection(collectionName);
+    return next();
+});
+
+app.get("/", function (req, response) {
+    response.sendFile(path.join(__dirname, '/../index.html'));
+});
+
+app.get('/collection/:collectionName', (req, response, next) => {
+    req.collection.find({}).toArray((error, results) => {
+        if (error) return next(error)
+        response.send(results)
+    })
 })
 
-app.use(function (request, response, next) {
-    console.log("Request IP: " + request.url);
+app.use(function (req, response, next) {
+    console.log("Request IP: " + req.url);
     console.log("Request date: " + new Date());
     next();
 });
 
-
-
 // var imagePath = path.resolve(__dirname,"../Images");
 // app.use(express.static(imagePath));
 
-app.use(function (request, response, next) {
-    var filePath = path.join(__dirname, "/..", request.url);
+app.use(function (req, response, next) {
+    var filePath = path.join(__dirname, "/..", req.url);
     console.log(filePath);
     fs.stat(filePath, function (error, fileInfo) {
         if (error) {
@@ -40,14 +55,11 @@ app.use(function (request, response, next) {
     });
 });
 
-app.get("/", function (request, response) {
-    response.sendFile(path.join(__dirname, '/../index.html'));
-});
 
-app.use(function (request, response) {
+
+app.use(function (req, response) {
     response.status(404).send("This page has not been made yet!");
 });
 
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
-});
+const port = process.env.PORT || 3000;
+app.listen(port);
